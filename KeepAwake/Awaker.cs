94b369121ace26a6a3b69b60147264667f02e691
unknown <gou@.(none)>
2011-08-25 21:01:53 +0800

    first commit

---
+++ KeepAwake/Awaker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace KeepAwake
+{
+    class Awaker
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        public bool isStarted
+        {
+            get { return worker != null; }
+        }
+
+        Thread worker;
+        public void Start()
+        {
+            if (isStarted) return;
+            worker = new Thread(worker_thread);
+            worker.Start();
+        }
+        public void Stop()
+        {
+            worker.Abort();
+            worker = null;
+        }
+        void worker_thread()
+        {
+            while (true)
+            {
+                SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+                Console.WriteLine("awaked!");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ KeepAwake/Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KeepAwake
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var program = new Program();
+            program.run();
+        }
+
+        NotifyIcon notifyicon;
+        MenuItem mtNoScreenSaver, mtNoScreenShutdown, mtNoSystemSleep;
+        MenuItem mtForever, mtTiming, mtStart, mtExit;
+        public void run()
+        {
+            var menu = new ContextMenu();
+            mtNoScreenSaver = menu.MenuItems.Add("阻止屏保", NoScreenSaver);
+            mtNoScreenShutdown = menu.MenuItems.Add("阻止关闭屏幕", NoScreenShutdown);
+            mtNoSystemSleep = menu.MenuItems.Add("阻止待机（睡眠）", NoSystemSleep);
+            mtNoScreenSaver.Enabled = false;
+            mtNoScreenShutdown.Enabled = false;
+            mtNoSystemSleep.Checked = true;
+            menu.MenuItems.Add("-");
+
+            mtForever = menu.MenuItems.Add("长期", Forever);
+            mtTiming = menu.MenuItems.Add("计时", Timing);
+            mtForever.Checked = true;
+            mtTiming.Enabled = false;
+            menu.MenuItems.Add("-");
+
+            foreach (var item in menu.MenuItems)
+                (item as MenuItem).RadioCheck = true;
+
+            mtStart = menu.MenuItems.Add("开始", OnStartStop);
+            mtExit = menu.MenuItems.Add("退出", OnExit);
+            mtStart.Tag = "停止";
+
+            notifyicon = new NotifyIcon();
+            notifyicon.ContextMenu = menu;
+            notifyicon.Visible = true;
+            notifyicon.Icon = SystemIcons.Shield;
+            Application.Run();
+        }
+
+        private void NoScreenSaver(object sender, EventArgs e)
+        {
+            mtNoScreenSaver.Checked = true;
+            mtNoScreenShutdown.Checked = false;
+            mtNoSystemSleep.Checked = false;
+        }
+        private void NoScreenShutdown(object sender, EventArgs e)
+        {
+            mtNoScreenSaver.Checked = false;
+            mtNoScreenShutdown.Checked = true;
+            mtNoSystemSleep.Checked = false;
+        }
+        private void NoSystemSleep(object sender, EventArgs e)
+        {
+            mtNoScreenSaver.Checked = false;
+            mtNoScreenShutdown.Checked = false;
+            mtNoSystemSleep.Checked = true;
+        }
+        private void Forever(object sender, EventArgs e)
+        {
+            mtForever.Checked = true;
+            mtTiming.Checked = false;
+        }
+        private void Timing(object sender, EventArgs e)
+        {
+            mtForever.Checked = false;
+            mtTiming.Checked = true;
+        }
+
+        Awaker awaker = new Awaker();
+        private void OnStartStop(object sender, EventArgs e)
+        {
+            var s = mtStart.Tag as string;
+            mtStart.Tag = mtStart.Text;
+            mtStart.Text = s;
+            if (awaker.isStarted) awaker.Stop();
+            else awaker.Start();
+        }
+        private void OnExit(object sender, EventArgs e)
+        {
+            awaker.Stop();
+            notifyicon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
